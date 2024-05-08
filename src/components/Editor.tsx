130d369a9e3f6009ff30ebee9466d9941d90adeb
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-05-08 00:15:41 -0400

    testing some stuff

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,27 +5,43 @@ import EditableHeading from "./EditableHeading";
 import EditableList from "./EditableList";
 import EditableTwoSide from "./EditableTwoSide";
 import Experience from "./section-items/Experience";
+import useResumeEditorStore from "@/stores/useResumeEditorStore";
 
 const Editor: FC = () => {
     // these are all temporary
-    const [title, setTitle] = useState<string>("Education");
-    const [items, setItems] = useState<string[]>([]);
-
-    const [topLeft, setTopLeft] = useState<string>("<b></b>");
-    const [topRight, setTopRight] = useState<string>("<b></b>");
-    const [bottomLeft, setBottomLeft] = useState<string>("<i></i>");
-    const [bottomRight, setBottomRight] = useState<string>("<i></i>");
-
+    const sections = useResumeEditorStore((state) => state.sections);
     return (
         <div className="bg-white text-black p-4">
+            {sections.map((section, index) => {
+                return (
+                    <div key={index} className="mb-4">
+                        <EditableHeading content={section.title} setContent={(content) => {}} />
+                        {section.items.map((item, index) => {
+                            if (item.type === "experience") {
+                                return (
+                                    <Experience
+                                        key={index}
+                                        company={{ content: item.value.company, setContent: (content) => {} }}
+                                        position={{ content: item.value.position, setContent: (content) => {} }}
+                                        date={{ content: item.value.date, setContent: (content) => {} }}
+                                        location={{ content: item.value.location, setContent: (content) => {} }}
+                                        description={{ content: item.value.description, setContent: (content) => {} }}
+                                    />
+                                );
+                            }
+                        })}
+                    </div>
+                );
+            })}
+            {/* <EditableHeading content={name} setContent={setName} />
             {/* <EditableHeading content={title} setContent={setTitle} /> */}
-            <Experience
+            {/* <Experience
                 company={{ content: topLeft, setContent: setTopLeft }}
                 position={{ content: bottomLeft, setContent: setBottomLeft }}
                 date={{ content: bottomRight, setContent: setBottomRight }}
                 location={{ content: topRight, setContent: setTopRight }}
                 description={{ content: items, setContent: setItems }}
-            />
+            /> */}
         </div>
     );
 };
--- src/stores/useResumeEditorStore.ts
+++ src/stores/useResumeEditorStore.ts
@@ -1,19 +1,42 @@
 import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
 
 // type: education/experience, custom, project
+interface SectionTypes {
+    experience: {
+        company: string;
+        position: string;
+        date: string;
+        location: string;
+        description: string[];
+    };
+    education: {
+        school: string;
+        degree: string;
+        date: string;
+        location: string;
+    };
+}
+
+type SectionItem = {
+    [K in keyof SectionTypes]: {
+        type: K;
+        id: string;
+        value: SectionTypes[K];
+    };
+}[keyof SectionTypes];
+
 interface ResumeEditor {
     name: string;
     subtitle: string;
     contact: string;
 
     sections: {
+        id: string;
         title: string;
-        items: {
-            type: string;
-        }[];
+        items: SectionItem[];
     }[];
 }
-
 const useResumeEditorStore = create<ResumeEditor>((set) => ({
     name: "John Doe",
     subtitle: "Software Engineer",
@@ -23,9 +46,32 @@ const useResumeEditorStore = create<ResumeEditor>((set) => ({
     sections: [
         {
             title: "Education",
+            id: uuidv4(),
             items: [
                 {
+                    type: "experience",
+                    id: uuidv4(),
+                    value: {
+                        company: "Amazon",
+                        position: "Software Engineer",
+                        date: "August 2017 - May 2021",
+                        location: "Berkeley, CA",
+                        description: [
+                            "Developed a new feature that increased user retention by 20%",
+                            "Implemented a new algorithm that reduced server load by 15%",
+                            "Collaborated with a team of 5 to develop a new product from scratch",
+                        ],
+                    },
+                },
+                {
                     type: "education",
+                    id: uuidv4(),
+                    value: {
+                        date: "May 2024",
+                        degree: "Master of Science in Computer Science",
+                        location: "Berkeley, CA",
+                        school: "University of California, Berkeley",
+                    },
                 },
             ],
         },
