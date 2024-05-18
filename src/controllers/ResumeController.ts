055ce207606e8d27b92b4c46c0cd5989edb7b495
j <77560326+Jxl-s@users.noreply.github.com>
2024-05-18 12:27:15 -0400

    add a controller file for later use

---
+++ src/controllers/ResumeController.ts
@@ -0,0 +1,6 @@
+export const ResumeController = {
+    async askAi(req: Request, res: Response) {},
+
+    async exportResume(req: Request, res: Response) {},
+    async importResume(req: Request, res: Response) {},
+};
