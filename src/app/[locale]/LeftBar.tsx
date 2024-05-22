257e92876346cc3feb76fcfee1f5d0e3d8bd2f63
j <77560326+Jxl-s@users.noreply.github.com>
2024-05-21 21:16:33 -0400

    refactor left bar

--- src/app/[locale]/LeftBar.tsx
+++ src/app/[locale]/LeftBar.tsx
@@ -8,6 +8,84 @@ import useDocSettingsStore, {
 } from "@/stores/useDocSettingsStore";
 import { Unit } from "@/types/unit";
 
+interface LeftInputProps {
+    title: string;
+    value: number;
+    setValue: (value: number) => void;
+    unit?: string;
+    className?: string;
+}
+
+const LeftInput: FC<LeftInputProps> = ({
+    title,
+    value,
+    setValue,
+    unit,
+    className,
+}) => {
+    return (
+        <div
+            className={`grid grid-cols-2 gap-2 justify-between items-center ${className}`}
+        >
+            <span className={unit ? "" : "ms-2"}>{title}</span>
+            <div className="bg-dark2 rounded-lg py-2 px-4 flex justify-between gap-2">
+                <input
+                    className="bg-inherit w-full"
+                    type="number"
+                    value={value}
+                    onChange={(e) => setValue(Number(e.target.value))}
+                />
+                {unit && <span className="opacity-50">{unit}</span>}
+            </div>
+        </div>
+    );
+};
+
+interface TwoInputsProps {
+    title: string;
+    value1: number;
+    value2: number;
+    setValue1: (value: number) => void;
+    setValue2: (value: number) => void;
+    unit?: string;
+    className?: string;
+}
+
+const TwoInputs: FC<TwoInputsProps> = ({
+    title,
+    value1,
+    value2,
+    setValue1,
+    setValue2,
+    unit,
+}) => {
+    return (
+        <>
+            <p className="mt-2">{title}</p>
+            <div className="flex w-full mt-1 gap-3">
+                <div className="bg-dark2 rounded-lg py-2 px-4 flex gap-2">
+                    <input
+                        className="bg-inherit w-full"
+                        type="number"
+                        value={value1}
+                        onChange={(e) => setValue1(Number(e.target.value))}
+                    />
+                    <span className="opacity-50">{unit}</span>
+                </div>
+                <div className="bg-dark2 rounded-lg py-2 px-4 flex gap-2">
+                    <input
+                        className="bg-inherit w-full"
+                        type="number"
+                        value={value2}
+                        onChange={(e) => setValue2(Number(e.target.value))}
+                    />
+                    <span className="opacity-50">{unit}</span>
+                </div>
+            </div>
+        </>
+    );
+};
+
 const LeftBar: FC = () => {
     const [jobDescription, setJobDescription] = useResumeEditorStore(
         (state) => [state.jobDescription, state.setJobDescription]
@@ -79,15 +157,12 @@ const LeftBar: FC = () => {
     return (
         <section className="w-full min-w-[306px]">
             <article className="bg-dark1 rounded-lg p-2 w-full print:hidden mb-2 block">
-                <div className="grid grid-cols-2 gap-2 justify-between items-center mb-2">
-                    <span className="ms-2">Spacing</span>
-                    <input
-                        className="bg-dark2 w-full rounded-lg py-2 px-4"
-                        type="number"
-                        value={spacing}
-                        onChange={(e) => setSpacing(Number(e.target.value))}
-                    />
-                </div>
+                <LeftInput
+                    title="Spacing"
+                    value={spacing}
+                    setValue={setSpacing}
+                    className="my-2"
+                />
                 <div className="grid grid-cols-2 gap-2 justify-between items-center">
                     <span className="ms-2">Unit</span>
                     <div className="bg-dark2 rounded-lg py-2 px-4 flex justify-between gap-2">
@@ -123,91 +198,46 @@ const LeftBar: FC = () => {
             </article>
             <article className="bg-dark1 rounded-lg p-4 w-full print:hidden mb-2 block">
                 <h1 className="text-2xl font-semibold">Margins</h1>
-                <p className="mt-2">Top-Bottom Margins</p>
-                <div className="flex w-full mt-1 gap-3">
-                    <div className="bg-dark2 rounded-lg py-2 px-4 flex gap-2">
-                        <input
-                            className="bg-inherit w-full"
-                            type="number"
-                            value={convertToUnit(marginTop, multiplierUnit)}
-                            onChange={(e) => setMTop(Number(e.target.value))}
-                        />
-                        <span className="opacity-50">{multiplierUnit}</span>
-                    </div>
-                    <div className="bg-dark2 rounded-lg py-2 px-4 flex gap-2">
-                        <input
-                            className="bg-inherit w-full"
-                            type="number"
-                            value={convertToUnit(marginBottom, multiplierUnit)}
-                            onChange={(e) => setMBottom(Number(e.target.value))}
-                        />
-                        <span className="opacity-50">{multiplierUnit}</span>
-                    </div>
-                </div>
-                <p className="mt-2">Left-Right Margins</p>
-                <div className="flex w-full mt-1 gap-3">
-                    <div className="bg-dark2 rounded-lg py-2 px-4 flex gap-2">
-                        <input
-                            className="bg-inherit w-full"
-                            type="number"
-                            value={convertToUnit(marginLeft, multiplierUnit)}
-                            onChange={(e) => setMLeft(Number(e.target.value))}
-                        />
-                        <span className="opacity-50">{multiplierUnit}</span>
-                    </div>
-                    <div className="bg-dark2 rounded-lg py-2 px-4 flex gap-2 justify-between">
-                        <input
-                            className="bg-inherit w-full"
-                            type="number"
-                            value={convertToUnit(marginRight, multiplierUnit)}
-                            onChange={(e) => setMRight(Number(e.target.value))}
-                        />
-                        <span className="opacity-50">{multiplierUnit}</span>
-                    </div>
-                </div>
+                <TwoInputs
+                    title="Top-Bottom Margins"
+                    value1={marginTop}
+                    value2={marginBottom}
+                    setValue1={setMTop}
+                    setValue2={setMBottom}
+                    unit={multiplierUnit}
+                />
+                <TwoInputs
+                    title="Left-Right Margins"
+                    value1={marginLeft}
+                    value2={marginRight}
+                    setValue1={setMLeft}
+                    setValue2={setMRight}
+                    unit={multiplierUnit}
+                />
             </article>
             <article className="bg-dark1 rounded-lg p-4 w-full print:hidden mb-2 block">
                 <h1 className="text-2xl font-semibold">Font Sizes</h1>
-                <div className="grid grid-cols-2 gap-2 mt-2 justify-between items-center">
-                    <span>Title Size</span>
-                    <div className="bg-dark2 rounded-lg py-2 px-4 flex justify-between gap-2">
-                        <input
-                            className="bg-inherit w-full"
-                            type="number"
-                            value={convertToUnit(titleSize, multiplierUnit)}
-                            onChange={(e) => setSTitle(Number(e.target.value))}
-                        />
-                        <span className="opacity-50">{multiplierUnit}</span>
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-2 mt-2 justify-between items-center">
-                    <span>Heading Size</span>
-                    <div className="bg-dark2 rounded-lg py-2 px-4 flex justify-between gap-2">
-                        <input
-                            className="bg-inherit w-full"
-                            type="number"
-                            value={convertToUnit(headingSize, multiplierUnit)}
-                            onChange={(e) =>
-                                setSHeading(Number(e.target.value))
-                            }
-                        />
-                        <span className="opacity-50">{multiplierUnit}</span>
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-2 mt-2 justify-between items-center">
-                    <span>Content Size</span>
-                    <div className="bg-dark2 rounded-lg py-2 px-4 flex justify-between gap-2">
-                        <input
-                            className="bg-inherit w-full"
-                            type="number"
-                            value={convertToUnit(contentSize, multiplierUnit)}
-                            onChange={(e) =>
-                                setSContent(Number(e.target.value))
-                            }
-                        />
-                        <span className="opacity-50">{multiplierUnit}</span>
-                    </div>
-                </div>
+                <LeftInput
+                    title="Title Size"
+                    value={convertToUnit(titleSize, multiplierUnit)}
+                    setValue={setSTitle}
+                    unit={multiplierUnit}
+                    className="mt-2"
+                />
+                <LeftInput
+                    title="Heading Size"
+                    value={convertToUnit(headingSize, multiplierUnit)}
+                    setValue={setSHeading}
+                    unit={multiplierUnit}
+                    className="mt-2"
+                />
+                <LeftInput
+                    title="Content Size"
+                    value={convertToUnit(contentSize, multiplierUnit)}
+                    setValue={setSContent}
+                    unit={multiplierUnit}
+                    className="mt-2"
+                />
             </article>
         </section>
     );
