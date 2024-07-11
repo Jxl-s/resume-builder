12fb1e6c4522036b21864ce5a268412bce60f850
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-07-10 22:21:52 -0400

    prepare left and top i18n strings

--- src/i18n/messages/en.ts
+++ src/i18n/messages/en.ts
@@ -1,7 +1,22 @@
 const messages = {
-    Index: {
-        title: "Index",
-        description: "This is the index page",
+    Top: {
+        resume: "Resume",
+    },
+    Left: {
+        spacing: "Spacing",
+        unit: "Unit",
+        job_description: "Job Description",
+        job_description_desc:
+            "This will give the AI more context when giving suggestions.",
+
+        margins: "Margins",
+        top_bottom_margins: "Top-Bottom Margins",
+        left_right_margins: "Left-Right Margins",
+
+        font_sizes: "Font Sizes",
+        title_size: "Title Size",
+        heading_size: "Heading Size",
+        content_size: "Content Size",
     },
 };
 
